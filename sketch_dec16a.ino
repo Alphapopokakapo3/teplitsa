62bb47a69a5120f46147ebf085f6a5d055058c99
Alphapopokakapo <68771756+Alphapopokakapo3@users.noreply.github.com>
2025-01-24 12:22:25 +0300

    Add files via upload

--- sketch_dec16a.ino
+++ sketch_dec16a.ino
@@ -1,26 +1,31 @@
-int counter = 0;
-int mois_sum = 0;
 int mois;
-
+int mois_sum = 0;
+int counter = 0;
+int mois_av;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(34, INPUT);
+  pinMode(21, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  delay(200);
-  counter++;
-  mois_sum = mois_sum + analogRead(34);
+  counter ++;
+  mois = analogRead(34);
+  mois_sum = mois_sum + mois;
+  
   if (counter == 5) {
-    mois = mois_sum/5;
-    Serial.println(mois);
-    counter = 0;
-    mois_sum = 0;
-    }
-
-   
-
-
+    mois_av = mois_sum/counter;
+    Serial.println(mois_av);
+  
+    if (mois_av > 300) {
+      digitalWrite(21, 1);
+      }
+    else {
+      digitalWrite(21, 0);
+      }
+      counter = 0;
+      mois_sum = 0;
+  }
 }
